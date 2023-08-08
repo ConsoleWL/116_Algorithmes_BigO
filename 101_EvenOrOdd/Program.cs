36b22b29bfdc570d97812ac92bcba4100453809f
Nikita Obergan <Obergannikita@gmail.com>
2023-08-08 12:19:40 -0400

    initial

---
+++ 101_EvenOrOdd/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _101_EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
