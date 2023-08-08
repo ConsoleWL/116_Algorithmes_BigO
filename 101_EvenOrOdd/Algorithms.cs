b88fedd21dc62dad93a609ce5817a9985e4cdabe
Nikita Obergan <Obergannikita@gmail.com>
2023-08-08 14:50:26 -0400

    fixed all sort method, add repeatedNames()

--- 101_EvenOrOdd/Algorithms.cs
+++ 101_EvenOrOdd/Algorithms.cs
@@ -8,7 +8,7 @@ namespace _101_EvenOrOdd
 {
     public class Algorithms
     {  
-        // O(1)
+        // O(1) aka constant
         public bool EvenOrOdd(int number)
         {
             if (number % 2 == 0)
@@ -16,18 +16,68 @@ namespace _101_EvenOrOdd
             return false;
         }
 
-        // O(n);
+        // O(n) aka linear
         public bool LessThan100(int[] numbers)
         {
             bool result = true;
 
             for (int i = 0; i < numbers.Length; i++)
             {
-                if (i >= 100)
+                if (numbers[i] >= 100)
                     result = false;
             }
 
             return result;
+        }
+
+        //string[] names1 = new[] { "Tom", "Mike", "Jorge", "Tom" };
+        //string[] names2 = new[] { "Tom", "Mike", "Jorge", "Bob" };
+
+        // I think is complexity here is O(n log n) aka N-log-N
+        // because with each iteration amount of names we have to go in the second loop decreases
+        
+        public bool RepeatedNames(string[] names)
+        {
+            bool result = false;
+            int counter = 1;
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                for (int j = counter; j < names.Length; j++)
+                {
+                    if (names[i] == names[j])
+                    {
+                        result = true;
+                        i = names.Length;
+                        j = names.Length;
+
+                    }
+                }
+                counter++;
+            }
+
+            return result;
+        }
+
+        // O(n^2) aka quadratic
+        public int[] SortList(int[] numbers)
+        {
+            int temp;
+
+            for (int i = 0; i < numbers.Length - 1 ; i++)
+            {
+                for (int j = 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] > numbers[j])
+                    {
+                        temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
+                    }
+                }
+            }
+
+            return numbers;
         }
     }
 }
--- 101_EvenOrOdd/Program.cs
+++ 101_EvenOrOdd/Program.cs
@@ -9,6 +9,23 @@
             bool result1 = algorithms.EvenOrOdd(5);
             bool result2 = algorithms.EvenOrOdd(6);
 
+            int[] numbers1 = new[] { 1, 5, 6, 7, };
+            int[] numbers2 = new[] { 1, 5, 6, 7, 198 };
+
+            bool names1Result = algorithms.LessThan100(numbers1);
+            bool names2Result = algorithms.LessThan100(numbers2);
+
+            string[] names1 = new[] {"Tom", "Mike", "Jorge", "Tom"};
+            string[] names2 = new[] {"Tom", "Mike", "Jorge", "Bob"};
+
+            bool resultNames1 = algorithms.RepeatedNames(names1);
+            bool resultNames2 = algorithms.RepeatedNames(names2);
+
+            int[] numbersUnsoreted = new[] { 1, 7, 3, 6, 100, 32 };
+            int[] resultSorted = algorithms.SortList(numbersUnsoreted);
+
+            
+
             int temp = 0;
         }
     }
